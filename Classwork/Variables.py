d15d7c23883d172c47c66dc1418608f32400fda0
ShreyasPoduri <shreyaspoduri@gmail.com>
2025-03-06 18:41:01 +0100

    new file:   Classwork/Activity1.py 	new file:   Classwork/operators.py

---
+++ Classwork/Variables.py
@@ -0,0 +1,23 @@
+name = "Shreyas"
+Age = 12
+is_Student = True
+weight = 35.5
+
+print("Name :",name)
+print("Data type of name is :",type(name))
+
+print("Age :",Age)
+print("Data type of Age is :",type(Age))
+
+print("Is student :",is_Student)
+print("Data type of is_Student is :",type(is_Student))
+
+print("Name :",weight)
+print("Data type of weight is :",type(weight))
+
+print("/n After type casting")
+
+Age = str(Age)
+print(Age)
+is_Student = int(is_Student)
+print(is_Student)
---
+++ Classwork/operators.py
@@ -0,0 +1,16 @@
+num1 = 3
+num2 = 20
+
+print("Number 1:",num1)
+print("Number 2:",num2)
+print("Addition:",num1+num2)
+print("subraction:",num1-num2)
+print("multiplictation:",num1*num2)
+print("division:",num1/num2)
+print("floor division:",num1//num2)
+print("Equal: ?",num1==num2)
+print("Num1 is greater?:",num1>num2)
+print("Num2 is greater?:",num1<num2)
+print("Not equal?:",num1!=num2)
+result = num1//num2+num2**2+10
+print("The result of this equation is:",result)
