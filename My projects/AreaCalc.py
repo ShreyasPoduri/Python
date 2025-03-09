fc0c904ad792588c655fd646cf1ab2c3e6ffc679
ShreyasPoduri <shreyaspoduri@gmail.com>
2025-03-09 15:00:23 +0100

    turtle

---
+++ My projects/AreaCalc.py
@@ -0,0 +1,8 @@
+lenght=float(input("Enter the length of a rectangle"))
+
+width= float(input("Enter the width of a rectangle"))
+
+area= lenght * width
+
+print(f"Area is: {area} cm^2")
+
---
+++ My projects/madLibs.py
@@ -0,0 +1,15 @@
+adjective1= input("Enter an adjective: ")
+
+noun= input("Enter an noun: ")
+
+adjective2= input("Enter an adjective: ")
+
+verb= input("Enter an verb: ")
+
+adjective3= input("Enter an adjective: ")
+
+
+print(f"Today i went to a {adjective1} park")
+print(f"Near a tree, I saw a {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")   
+print(f"I was {adjective3}") 
